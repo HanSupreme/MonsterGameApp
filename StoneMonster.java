8c231e52af6e18623d37126bb68449f451bb2d4d
Séyhan A. Cileli <52051065+HanSupreme@users.noreply.github.com>
2019-12-02 12:15:27 -0500

    Add files via upload

---
+++ StoneMonster.java
@@ -0,0 +1,13 @@
+package Monster;
+
+public class StoneMonster extends Monster {
+
+	public StoneMonster(String string) {
+		super(string);
+	}
+
+	public String attack() {
+		String attack = "Attack with stones!";
+		return attack;
+	}
+}
